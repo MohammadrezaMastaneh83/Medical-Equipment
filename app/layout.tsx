15f40e29411ef0dc23648d25848e2773fdcaa9b7
MohammadrezaMastaneh <mohammadreza.mastaneh4@gmail.com>
2023-10-24 11:14:59 +0330

    complete the header and some font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { constants } from 'buffer'
+import localFont from 'next/font/local'
 
 const inter = Inter({ subsets: ['latin'] })
+const medium = localFont({
+  src:"/IRANSansX-Medium.ttf"
+  
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from 'next/image'
-
+import styles from "./page.module.css"
+import Header from "./components/header"
 export default function Home() {
   return (
-    <div></div>
+    <div className={styles.background}>
+      <Header/>
+    </div>
          
   )
 }
